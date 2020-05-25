345ddb9ae5cbd2d7aaf1929a08d8632127e5f862
Nursultan <eenurike@gmail.com>
2020-05-25 21:55:34 +0600

    responsive nav & header - done

--- src/components/LandingPageBlocks/HeroBlock/HeroBlock.js
+++ src/components/LandingPageBlocks/HeroBlock/HeroBlock.js
@@ -4,27 +4,36 @@ import background from "../../../image/bg.png";
 import working from "../../../image/working.png";
 import arrow1 from "../../../image/Arrow1.png";
 import arrow2 from "../../../image/Arrow2.png";
+import mobileArrowLeft from "../../../image/mobileArrowLeft.png";
+import mobileArrowRight from "../../../image/mobileArrowRight.png";
+import mobileBg from "../../../image/mobileBg.png";
+
 
 const HeroBlock = props => {
     return (
         <section>
 
-			 <img src={background} class="large_image" alt=""/> 
+			 <img src={background} className="large_image" alt=""/> 
+			 <img src={mobileBg} className="mobile_image" alt=""/> 
 
 
 					<div className="grid_wrapper">
 
 
 						<div className="section_title">
-						Let our pre-vetted developers build a <span>Microservice</span> for you
+						<h1>Let our pre-vetted developers build a <span>Microservice</span> for you</h1>
 						</div>
 
 						<div className="section_text">
 							<p>We are the first freelance platform that invests in education of our freelancers so that they can provide a better service with clear deliverables.</p>
 
 							<div className="arrow">
-								<img src={arrow2} alt=""/>
-								<img src={arrow1} alt=""/>
+								<img src={arrow2} className="ArrowLeft large"alt=""/>
+								<img src={arrow1} className="ArrowRight large"alt=""/>
+								<img src={mobileArrowLeft} className="ArrowLeft mobile"alt=""/>
+								<img src={mobileArrowRight} className="ArrowRight mobile"alt=""/>
+
+
 							</div>
 						</div>
 
--- src/components/NavigationBar/NavigationBar.js
+++ src/components/NavigationBar/NavigationBar.js
@@ -1,23 +1,27 @@
 import React from 'react';
 import './NavigationBar.css';
 import logo from "../../image/logo.png";
+import mobileLogo from "../../image/mobileLogo.png";
 
 
 
 const NavigationBar = () => {
     return (
         <nav className="Navigation-Panel">
-            <div class="wrapper">
-							<div class="nav_wrapper">
-								<div class="logo">
+            <div className="wrapper">
+							<div className="nav_wrapper">
+								<div className="logo">
 									<img src={logo}></img>
+									<img src={mobileLogo}></img>
 								</div>
 
 								<div className="navigation">
-									<a href="#">What we do</a>
-									<a href="#">How it works</a>
-									<a href="#">About us</a>
-									<a href="#">Get started</a>
+									<div className="nav_desctop">
+										<a href="#">What we do</a>
+										<a href="#">How it works</a>
+										<a href="#">About us</a>
+										<a href="#">Get started</a>
+									</div>
 									<span className="mobile_nav">
 
 									</span>
