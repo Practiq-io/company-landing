fa5bd8a36e24858e34825a0f22176c5e6c05d39d
Timur <Timsonffs@gmail.com>
2020-06-01 15:29:25 +0600

    Refactored animation scripts (all data is stored in array of objects now)

--- src/components/LandingPageBlocks/HeroBlock/animationScripts/animationSript.js
+++ src/components/LandingPageBlocks/HeroBlock/animationScripts/animationSript.js
@@ -1,17 +1,42 @@
-import { code_first, code_second, code_third } from "./animationCodeText";
-import { title_first, title_second, title_third } from "./animationTitleText";
+let title_index = 0;
+let codeFlag = 0;
+let titleFlag = 0;
+let codeSpeed = 12;
+let titleSpeed = 35;
+const codeSnippets = [
+	{
+		title: "Microservice",
+		snippet: 
+`componentDidMount() { 
+  this.setState({ isLoading: true});
+			
+  fetch(PRACTIQ_API_BASE + WIZARD)
+    .then(response => {
+	 if (response.ok) {
+	   return buildWizard(response.json());
+	 } else {`,
+	},
+	{
+		title: "Design",
+		snippet: 
+`const UI_Component = props => { 
+  return(
+	<div>It all starts here!<div>
+	<p>Your first steps in programming</p>
+	<p>Supervised by specialists</p>
+  )`,
+	},
+	{
+		title: "React SPA",
+		snippet: 
+`This.State.Property = "New Array,
+  This.State.Default = "New Obj,
+  This.State.Value = {[e.target.name]:e.target.value}
+		 
+  Show Default Settings: true`,
+	},
+];
 
-const codeTextArr = [code_first, code_second, code_third];
-const titleTextArr = [title_first, title_second, title_third];
-
-var code_index = 0;
-var title_index = 0;
-var codeFlag = 0;
-var titleFlag = 0;
-var codeSpeed = 12;
-var titleSpeed = 35;
-var codeText = codeTextArr[code_index];
-var titleText = titleTextArr[title_index];
 
 const writeCode = (txt) => {
 	if (codeFlag < txt.length) {
@@ -35,20 +60,13 @@ const removeCode = (txt) => {
 		codeFlag--;
 		setTimeout(() => removeCode(txt), 10);
 	} else {
-		if (code_index === codeTextArr.length - 1) {
-			code_index = 0;
-			codeText = codeTextArr[code_index];
-		} else {
-			code_index++;
-			codeText = codeTextArr[code_index];
-		}
 		setTimeout(() => animationLogic(), 50);
 	}
 };
 
 const writeTitle = (txt) => {
 	if (titleFlag === 0) {
-		writeCode(codeText);
+		writeCode(codeSnippets[title_index].snippet); /////////////////////////
 	}
 	if (titleFlag < txt.length) {
 		document.getElementById(
@@ -67,22 +85,20 @@ const removeTitle = (txt) => {
 		titleFlag--;
 		setTimeout(() => removeTitle(txt), 35);
 	} else {
-		if (title_index === titleTextArr.length - 1) {
+		if(title_index === codeSnippets.length-1){
 			title_index = 0;
-			titleText = titleTextArr[title_index];
 		} else {
-			title_index++;
-			titleText = titleTextArr[title_index];
+			title_index++
 		}
 		setTimeout(() => animationLogic(), 10);
 	}
 };
 
 const animationLogic = () => {
-	if (titleFlag < titleText.length) {
-		setTimeout(() => writeTitle(titleText), 10);
-	} else if (titleFlag === titleText.length) {
-		setTimeout(() => removeTitle(titleText), 50);
+	if (titleFlag < codeSnippets[title_index].title.length) {
+		setTimeout(() => writeTitle(codeSnippets[title_index].title), 10);
+	} else if (titleFlag === codeSnippets[title_index].title.length) {
+		setTimeout(() => removeTitle(codeSnippets[title_index].title), 50);
 	}
 };
 
--- src/components/LandingPageBlocks/HeroBlock/animationScripts/animationTitleText.js
+++
@@ -1,3 +0,0 @@
-export const title_first = `Microservice`
-export const title_second = `Design`
-export const title_third = `React SPA`
