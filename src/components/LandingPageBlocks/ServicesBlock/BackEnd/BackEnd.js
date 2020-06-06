8c82a6648b9cc6f6a5ae4836ad1ef28881785ab6
Timur <Timsonffs@gmail.com>
2020-06-06 17:43:53 +0600

    fixed show/hide button in services cards

--- src/components/LandingPageBlocks/ServicesBlock/BackEnd/BackEnd.js
+++ src/components/LandingPageBlocks/ServicesBlock/BackEnd/BackEnd.js
@@ -11,38 +11,39 @@ const BackEnd = () => {
         <div className="backendCarouselFrame">
             
             <ServicesCarousel>
-               {/* <div className="carouselDiv"> */}
-                   <ServicesCard 
+               
+                   <ServicesCard
+                        id = {"api"}
                         imgPath = {API}
                         title = {"General purpose API"}
                         subTitle = {"Any kind of API service that is encapsulated and does any kind of operations."}
                         textBody = {"・Can be a back office API that just serves denormalized data from a number of other APIs or data storages."}
                    />
-               {/* </div>  */}
-               {/* <div className="carouselDiv"> */}
+               
                    <ServicesCard 
+                        id = {"connector"}
                         imgPath = {connector}
                         title = {"API Connector/Adapter"}
                         subTitle = {"Used a lot by a lot companies that need to connect to different APIs from the same product type and transform the data or operations to a single internal format."}
-                        textBody = {"・Silenium.inc is a company that uses multiple payment providers and based on a payment event or a call needs to make a request to one of the providers Lorem ipsum dolor sit amet consectetur adipisicing elit. Repellat amet modi, error nobis, quidem quas explicabo facere dolor saepe eaque tenetur fugiat. Ipsum est dicta amet fugiat ut culpa adipisci."}
+                        textBody = {"・Silenium.inc is a company that uses multiple payment providers and based on a payment event or a call needs to make a request to one of the providers <br>・Flywithus is a flights ticket seller and needs to integrate new airline by integrating with their API"}
                    />
-               {/* </div>
-               <div className="carouselDiv"> */}
-                   <ServicesCard 
+             
+                   <ServicesCard
+                        id = {"longprocess"}
                         imgPath = {longProcess}
                         title = {"Long running process"}
                         subTitle = {"Any kind of background process that is consuming data from some source and performing some operation upon it."}
                         textBody = {"・A process that is listening to a stock market, aggregating the data and saving it somewhere for further analysis."}
                    />
-               {/* </div>
-               <div className="carouselDiv"> */}
-                   <ServicesCard 
+               
+                   <ServicesCard
+                        id = {"script"}
                         imgPath = {longProcess}
-                        title = {"Long running process"}
-                        subTitle = {"Any kind of background process that is consuming data from some source and performing some operation upon it."}
-                        textBody = {"・A process that is listening to a stock market, aggregating the data and saving it somewhere for further analysis."}
+                        title = {"Script"}
+                        subTitle = {"General purpose script that is not deployed anywhere and can be executed on demand on any machine."}
+                        textBody = {"・A script that scrapes a competitors website to deduplicate products based on the existing base and add competitive pricing data. <br>・Script that parses Excel data and creates a certain report based on it (actual problem in audit firms. They have reporting departments for that)"}
                    />
-               {/* </div> */}
+              
             </ServicesCarousel>
 
         </div>
--- src/components/LandingPageBlocks/ServicesBlock/ServicesCard/ServicesCard.js
+++ src/components/LandingPageBlocks/ServicesBlock/ServicesCard/ServicesCard.js
@@ -1,61 +1,78 @@
-// import React from "react";
+// import React, { Component } from "react";
 // import "./ServicesCard.css";
 // import buttonRightArrow from "./ServicesCardImg/rightarrow.png";
 
-// const ServicesCard = (props) => {
+// class ServicesCard extends Component {
 
+// 	render(){
 
+// 		let hiddenString, displayString, toggleMarkup
+// 		displayString = this.props.textBody.slice(0, 150);
 
-//     let displayString = null;
-//     if(props.textBody){
-//         displayString = props.textBody.slice(0, 150);
-// 	}
-// 	console.log(displayString,"<== DISPLAYSTRING",displayString.length,"<== LENGTH");
-	
-//     // if(string.length > 150) {
-//     //     let cut = string.slice(150, string.length);
-//     //     console.log('\n',cut, "<===== im cuted string", cut.length, "<==== CUTED LENGTH\n");
-//     // }
-//     // console.log(props.textBody, "<== TEXT BODY STRING", props.textBody.length, "<===== TEXT LENGTH");
-	
-	
+// 		if(this.props.textBody.length > 150){
+// 			hiddenString = this.props.textBody.slice(150, this.props.textBody.length)
+// 			toggleMarkup = (
+// 				<span className="servicesCardMainSpan">
+// 					<span id="servicesCardDots">...</span>
+// 					<span id="moreText"></span>
+// 					<span onClick={() => toggle()} id="toggleTextSpan"> More</span>
+// 				</span>
+// 			)
+// 		}
 
+// 		const toggle = () => {
+// 			let dots = document.getElementById("servicesCardDots");
+// 			let moreText = document.getElementById("moreText");
+// 			moreText.innerHTML = hiddenString
+// 			let spanToggle = document.getElementById("toggleTextSpan");
 
+// 			if(dots.style.display === "none"){
+// 				dots.style.display = "inline";
+// 				spanToggle.innerHTML = " More";
+// 				moreText.style.display = "none";
+// 			} else {
+// 				dots.style.display = "none";
+// 				spanToggle.innerHTML = " Less";
+// 				moreText.style.display = "inline";
+// 			}
+// 		}
 
-// 	return (
-// 		<div className="servicesCardBody">
-// 			<div className="servicesCardImgbox">
-// 				<img
-// 					className="servicesCardImg"
-// 					src={props.imgPath}
-// 					alt="service icon"
-// 				/>
-// 			</div>
-
-// 			<div className="cardStructure">
-// 				<div className="cardInfoBox">
-// 					<div className="servicesCardUpperBox">
-// 						<p className="servicesCardTitle">{props.title}</p>
-// 						<p className="servicesCardSubTitle">{props.subTitle}</p>
-// 					</div>
-
-// 					<div className="servicesCardLowerBox">
-// 						<p className="servicesCardExample">Example:</p>
-// 						<p className="servicesCardTextBody">{props.textBody}</p>
-// 					</div>
-// 				</div>
-
-// 				<div className="servicesCardButton">
-// 					<p>Get started</p>
+// 		return (
+// 			<div className="servicesCardBody">
+// 				<div className="servicesCardImgbox">
 // 					<img
-// 						className="servicesCardButtonImg"
-// 						src={buttonRightArrow}
-// 						alt="action button"
+// 						className="servicesCardImg"
+// 						src={this.props.imgPath}
+// 						alt="service icon"
 // 					/>
 // 				</div>
+
+// 				<div className="cardStructure">
+// 					<div className="cardInfoBox">
+// 						<div className="servicesCardUpperBox">
+// 							<p className="servicesCardTitle">{this.props.title}</p>
+// 							<p className="servicesCardSubTitle">{this.props.subTitle}</p>
+// 						</div>
+
+// 						<div className="servicesCardLowerBox">
+// 							<p className="servicesCardExample">Example:</p>
+// 							<p className="servicesCardTextBody">{displayString}{hiddenString ? toggleMarkup : null}</p>
+// 						</div>
+// 					</div>
+
+// 					<div className="servicesCardButton">
+// 						<p>Get started</p>
+// 						<img
+// 							className="servicesCardButtonImg"
+// 							src={buttonRightArrow}
+// 							alt="action button"
+// 						/>
+// 					</div>
+// 				</div>
 // 			</div>
-// 		</div>
-// 	);
+// 		);
+// 	}
+
 // };
 
 // export default ServicesCard;
@@ -64,44 +81,36 @@ import React, { Component } from "react";
 import "./ServicesCard.css";
 import buttonRightArrow from "./ServicesCardImg/rightarrow.png";
 
-
 class ServicesCard extends Component {
 
-
+	state = {
+		show: false,
+		string: this.props.textBody,
+		hiddenString: false,
+		displayedString: false
+	}
 	
-	
+	componentDidMount(){
 
-	render(){
+		let cut = this.state.string.slice(150, this.state.string.length)
+		let display = this.state.string.slice(0, 150);
 
-		let hideString, displayString, toggleMarkup
-		displayString = this.props.textBody.slice(0, 150);
+		this.setState({hiddenString: cut});
+		this.setState({displayedString: display})
+	}
 
-		if(this.props.textBody.length > 150){
-			hideString = this.props.textBody.slice(150, this.props.textBody.length)
-			toggleMarkup = (
-				<span className="servicesCardMainSpan">
-					<span id="servicesCardDots">...</span>
-					<span id="moreText"></span>
-					<span onClick={() => toggle()} id="toggleTextSpan"> More</span>
-				</span>
-			)
-		}
+	tick = () => {
+		// this.setState({show: true})
+		// console.log("TRIGGERED");
+		this.setState({show: !this.state.show})
+		console.log("TRIGGERED");
+	}
 
-		const toggle = () => {
-			let dots = document.getElementById("servicesCardDots");
-			let moreText = document.getElementById("moreText");
-			moreText.innerHTML = hideString
-			let spanToggle = document.getElementById("toggleTextSpan");
-
-			if(dots.style.display === "none"){
-				dots.style.display = "inline";
-				spanToggle.innerHTML = " More";
-				moreText.style.display = "none";
-			} else {
-				dots.style.display = "none";
-				spanToggle.innerHTML = " Less";
-				moreText.style.display = "inline";
-			}
+	render() {
+		
+		{
+		console.log(this.state.hiddenString, "==== STATE CUT");
+		console.log(this.state.displayedString, "==== STATE DISPLAY");
 		}
 
 		return (
@@ -113,20 +122,38 @@ class ServicesCard extends Component {
 						alt="service icon"
 					/>
 				</div>
-	
+
 				<div className="cardStructure">
 					<div className="cardInfoBox">
 						<div className="servicesCardUpperBox">
 							<p className="servicesCardTitle">{this.props.title}</p>
 							<p className="servicesCardSubTitle">{this.props.subTitle}</p>
 						</div>
-	
+
 						<div className="servicesCardLowerBox">
 							<p className="servicesCardExample">Example:</p>
-							<p className="servicesCardTextBody">{displayString}{hideString ? toggleMarkup : null}</p>
+							<p className="servicesCardTextBody">
+								{this.state.displayedString}
+								<span className="servicesCardMainSpan"
+									style = {{
+										display: this.state.hiddenString ? "inline" : "none"
+									}}
+								>
+									<span id="servicesCardDots">...</span>
+									<span 
+										style = {{
+											display: this.state.show ? "inline" : "none"
+										}}
+										id="moreText">{this.state.hiddenString}
+									</span>
+									<span onClick={() => this.tick()} id="toggleTextSpan">
+										More	
+									</span>
+								</span>
+							</p>
 						</div>
 					</div>
-	
+
 					<div className="servicesCardButton">
 						<p>Get started</p>
 						<img
@@ -139,7 +166,6 @@ class ServicesCard extends Component {
 			</div>
 		);
 	}
-	
-};
+}
 
 export default ServicesCard;
