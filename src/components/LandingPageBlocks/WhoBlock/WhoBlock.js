421647a7b4de97c5d6f4a26fa1bd8cb24fdd40ff
eenurike <eenurike@gmail.com>
2020-06-09 16:20:12 +0600

    Who block final

--- src/components/LandingPageBlocks/WhoBlock/WhoBlock.js
+++ src/components/LandingPageBlocks/WhoBlock/WhoBlock.js
@@ -1,6 +1,11 @@
 import React from "react";
 import "./WhoBlock.css";
 import linkid from "../../../image/linkid.png";
+import IkaMobile from "../../../image/Ika-mobile.png";
+import LizaMobile from "../../../image/Liza-mobile.png";
+import RomaMobile from "../../../image/Roma-mobile.png";
+import linkidMobile from "../../../image/linkid-blue.png";
+
 
 const WhoBlock = (props) => {
 	return (
@@ -9,7 +14,7 @@ const WhoBlock = (props) => {
 				<h2>Who build this</h2>
 
 				<div className="person-card">
-					{/* <div className="person">
+					<div className="person">
 						<div className="person-image">
 							<p>Ilya Sidorovich</p>
 
@@ -22,9 +27,9 @@ const WhoBlock = (props) => {
 							Co-founder, Tech Senior Software Developer @ Booking.com BSc
 							Finance & Marketing, KIMEP
 						</p>
-					</div> */}
+					</div>
 
-					{/* <div className="person liza">
+					<div className="person liza">
 						<div className="person-image">
 							<p>Elizaveta Kolesnichenko</p>
 
@@ -37,9 +42,9 @@ const WhoBlock = (props) => {
 							Co-founder, Product / Strategy Head of Startup Incubation @ ACE
 							Incubator MSc Cognitive Neurobiology, UvA
 						</p>
-					</div> */}
+					</div>
 
-					{/* <div className="person roma">
+					<div className="person roma">
 						<div className="person-image">
 							<p>Roman Cherepivskiy</p>
 
@@ -52,7 +57,60 @@ const WhoBlock = (props) => {
 							Senior UI/UX Designer Computer Hardware and Software Almaty
 							technological university
 						</p>
-					</div> */}
+					</div>
+
+					<div className="person-mobile-block">
+						<div className="person-card-mobile ika">
+							<div className="person-card-image">
+								<img src={IkaMobile} alt=""/>
+							</div>
+
+							<div className="person-card-text">
+								<p>Ilya Sidorovich</p>
+								<p>Co-founder, Tech Senior Software Developer @ Booking.com
+									BSc Finance & Marketing, KIMEP</p>
+								<div className="person-card-logo">
+									<img src={linkidMobile} alt=""/>
+								</div>	
+							</div>
+
+						</div>
+
+						<div className="person-card-mobile liza">
+							<div className="person-card-image">
+								<img src={LizaMobile} alt=""/>
+							</div>
+
+							<div className="person-card-text">
+								<p>Elizaveta Kolesnichenko</p>
+								<p>Co-founder, Product / Strategy
+									Head of Startup Incubation 
+									@ ACE Incubator
+									MSc Cognitive Neurobiology, UvA</p>
+								<div className="person-card-logo">
+									<img src={linkidMobile} alt=""/>
+								</div>	
+							</div>
+
+						</div>
+
+						<div className="person-card-mobile roma">
+							<div className="person-card-image">
+								<img src={RomaMobile} alt=""/>
+							</div>
+
+							<div className="person-card-text">
+								<p>Ilya Sidorovich</p>
+								<p>Co-founder, Tech Senior Software Developer @ Booking.com
+									BSc Finance & Marketing, KIMEP</p>
+								<div className="person-card-logo">
+									<img src={linkidMobile} alt=""/>
+								</div>	
+							</div>
+
+						</div>
+						
+					</div>
 				</div>
 			</content>
 		</section>
