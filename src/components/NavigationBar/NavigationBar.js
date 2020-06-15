ff931b91a9840fd04e82c47b0722801b306a2d68
Timur <Timsonffs@gmail.com>
2020-06-15 22:34:57 +0600

    same weird comment

--- src/components/NavigationBar/NavigationBar.js
+++ src/components/NavigationBar/NavigationBar.js
@@ -2,33 +2,28 @@ import React, { Component } from "react";
 import "./NavigationBar.css";
 import logo from "../../image/logo.png";
 import mobileLogo from "../../image/mobileLogo.png";
-import MobileNavigation from './mobileNavigation/mobileNavigation';
+import MobileNavigation from "./mobileNavigation/mobileNavigation";
 
 class NavigationBar extends Component {
-
 	state = {
-		mobileNavigation: false
-	}
-
- 
+		mobileNavigation: false,
+	};
 
 	toggleMobileMenu = () => {
-		this.setState({mobileNavigation: !this.state.mobileNavigation})
-	}
-master
+		this.setState({ mobileNavigation: !this.state.mobileNavigation });
+	};
 
 	render() {
 		return (
 			<>
 				<nav className="Navigation-Panel">
-
 					<div className="mobile-navigation_display">
-						<MobileNavigation 
+						<MobileNavigation
 							display={this.state.mobileNavigation}
 							toggle={this.toggleMobileMenu}
 						/>
 					</div>
-					
+
 					<div className="wrapper">
 						<div className="nav_wrapper">
 							<div className="logo">
