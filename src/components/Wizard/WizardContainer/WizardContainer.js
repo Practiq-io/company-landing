a80d806be467678cc3443a180bf580e906b9c1f5
Timur <Timsonffs@gmail.com>
2020-08-02 17:55:45 +0600

    finished first version of wizard data for backend

--- src/components/Wizard/WizardContainer/WizardContainer.js
+++ src/components/Wizard/WizardContainer/WizardContainer.js
@@ -20,27 +20,100 @@ export class WizardContainer extends Component {
 		let taxonomy = data.taxonomy.selectedTags.map(item => {
 			return {skill: item[0], level: item[1]}
 		})
+
 		let specification = {}
+
 		if(data.specification.taskType === "Single page application"){
-			let spaData = data.specification.taskData.singlePageApplication;
-			let links = [spaData.designLink];
-			let attachments = spaData.attachedFiles;
+			let taskData = data.specification.taskData.singlePageApplication;
+			let links = [taskData.designLink];
+			let attachments = taskData.attachedFiles;
 			let apis = [];
-			Object.keys(spaData).forEach(key => {
+			Object.keys(taskData).forEach(key => {
 				if(key === "field" || key === "field2" || key === "field3" || key === "field4" || key === "field5"){
-					apis.push({apiField: spaData[key]})
+					apis.push({apiField: taskData[key]})
 				}
 			})
 			specification.links = links;
 			specification.attachments = attachments;
 			specification.apis = apis
 		}
+
+		if(data.specification.taskType === "Landing page"){
+			let taskData = data.specification.taskData.landingPage;
+			let links = [taskData.designLink];
+			let attachments = taskData.attachedFiles;
+			let apis = [];
+			Object.keys(taskData).forEach(key => {
+				if(key === "field" || key === "field2" || key === "field3" || key === "field4" || key === "field5"){
+					apis.push({apiField: taskData[key]})
+				}
+			})
+			specification.links = links;
+			specification.attachments = attachments;
+			specification.apis = apis
+		}
+
+		if(data.specification.taskType === "Frontend component"){
+			let taskData = data.specification.taskData.frontendComponent;
+			let links = [taskData.designLink];
+			let attachments = taskData.attachedFiles;
+			let apis = [];
+			Object.keys(taskData).forEach(key => {
+				if(key === "field" || key === "field2" || key === "field3" || key === "field4" || key === "field5"){
+					apis.push({apiField: taskData[key]})
+				}
+			})
+			specification.links = links;
+			specification.attachments = attachments;
+			specification.apis = apis
+		}
+
+		if(data.specification.taskType === "Custom frontend task"){
+			let taskData = data.specification.taskData.customFrontendTask;
+			specification.customFrontendTask = taskData.taskDescription;
+		}
+
+		if(data.specification.taskType === "General purpose API"){
+			let taskData = data.specification.taskData.generalApi;
+			let apis = [];
+			let requestResponse = {
+				request: taskData.apiRequest,
+				response: taskData.apiResponse
+			};
+			apis.push(requestResponse);
+			specification.apis = apis;
+		}
+
+		if(data.specification.taskType === "API Connector/Adapter"){
+			let taskData = data.specification.taskData.connectorAdapter;
+			let apis = [];
+			let requestResponse = {
+				request: taskData.apiRequest,
+				response: taskData.apiResponse,
+				action: taskData.documentation
+			};
+			apis.push(requestResponse);
+			specification.apis = apis;
+		}
+
+		if(data.specification.taskType === "Long running process"){
+			let taskData = data.specification.taskData.longRunningProcess;
+			specification.datasource = taskData.datasource;
+		}
+
+		if(data.specification.taskType === "Custom backend task"){
+			let taskData = data.specification.taskData.customBackendTask;
+			specification.customBackendTask = taskData.taskDescription;
+		}
+
 		let projectTimelines = [];
+
 		if(data.projectTimelines.customTimeline){
 			projectTimelines = data.projectTimelines.customTimeline
 		} else {
 			projectTimelines = data.projectTimelines.timeline
 		}
+
 		let backendData = {
 			user: {
 				email: data.aboutCompany.email,
@@ -51,8 +124,7 @@ export class WizardContainer extends Component {
 			type: data.specification.taskType,
 			requirements:{
 				taxonomy: taxonomy,
-				specification: specification,
-
+				specification: specification
 			},
 			deliverables : {
 				system: data.taxonomy.system,
@@ -60,6 +132,7 @@ export class WizardContainer extends Component {
 				projectTimelines: projectTimelines
 			}
 		}
+		console.log("Wizard data for backend :", backendData);
 	}
 
 	componentWillUnmount() {
